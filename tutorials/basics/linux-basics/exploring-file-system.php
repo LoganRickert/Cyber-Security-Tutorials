1e9e9626ebc18fd7ff2aeede8faf577928a10044
Logan Rickert <rickert.14@wright.edu>
2015-07-11 20:56:06 -0400

    Added exploring-file-system

--- tutorials/basics/linux-basics/exploring-file-system.php
+++ tutorials/basics/linux-basics/exploring-file-system.php
@@ -13,14 +13,26 @@
 
 ?>
                 <h3>Listing Files</h3>
-                <p>In order to see what files are in a directory, type 'ls' and press enter.</p>
-                <?PHP $code->printNextExample("Python"); ?>
+                <p>In order to see what files are in a directory, type 'ls' and press enter. You can 
+                    also type 'l' on some distros.</p>
+                <?PHP $code->printNextExample("Bash"); ?>
 
-                <h3>Links</h3>
-                <ul>
-                    <li><?PHP outLink("https://en.wikipedia.org/wiki/Linux", "Linux Wikipedia") ?>
-                    <li><?PHP outLink("http://distrowatch.com/", "DistroWatch") ?></li>
-                </ul>
+                <h3>Changing Directorys</h3>
+                <p>If you want to go into a directory, you can use 'cd'. 'cd' takes an argument, which 
+                    is the name of the directory. For example: 'cd Desktop'. If you want to go back 
+                    up a directory, you can use '..'. For example: 'cd ..'. If you type 'cd' with no 
+                    arguments, it goes back to your home directory, which is marked by a tilday, '~'.</p>
+                <?PHP $code->printNextExample("Bash"); ?>
+
+                <h3>Paths</h3>
+                <p>In Windows, the root directory is C:\. In Linux, the root directory is just /. If you 
+                    type pwd, you can see the directory of where you are at.
+                <?PHP $code->printNextExample("Bash"); ?>
+                <p>In Windows, your desktop is here: 'C:\Users\Logan\Desktop'. In Linux, your desktop is 
+                    here: '/home/logan/Desktop'. A relative path is a path that doesn't start with a / 
+                    while an absolute path does. A relative path is relative to where you are in your 
+                    file system, while an absoulte path always starts at the top and goes down.</p>
+                <?PHP $code->printNextExample("Bash"); ?>
 <?PHP
 
     File::dumpFile(
