e89edf2cbc5af59ee9dc630ad354a65cfddaa677
Logan Rickert <logan@rickertcorp.com>
2015-09-11 11:06:49 -0400

    Added env var draft

--- tutorials/basics/linux-basics/environment-variables.php
+++ tutorials/basics/linux-basics/environment-variables.php
@@ -5,6 +5,45 @@
         HEADER_HTML,
         array(
             "SITE_ROOT" => SITE_ROOT,
+            "title" => "Environment Variables",
+            "author" => "By Logan Rickert"
+        )
+    );
+    $code = new Code("code_examples/environment-variables.txt");
+
+?>
+                <h3>Environment Variables</h3>
+                <p>Environment variables are variables you can set that are relative to your shell. They can 
+                    help reduce long file paths to very short variable names. You can access a variable by doing
+                    $&lt;name&gt;. An important note is that you can't have any spaces. If you need to use spaces, you
+                    have to enclose the string in double quotes.</p>
+                <?PHP $code->printNextExample("Bash"); ?>
+                <p>You can also alias a word to a string of commands</p>
+                <?PHP $code->printNextExample("Bash"); ?>
+                <p>Every time you close your terminal, you lose the environment variables you set. If you want a set
+                    of enviroment variables to be set each time you open a new terminal session, you can edit
+                    your .rcbash. This can be found in your home directory. If you have a look at it, you'll see there is
+                    actually quite a lot in it, but here is a small snipet of it.</p>
+                <?PHP $code->printNextExample("Bash"); ?>
+
+<?PHP
+
+    File::dumpFile(
+        FOOTER_HTML,
+        array(
+            "previous" => "file-permissions",
+            "next" => "environment-variables"
+        )
+    );
+
+?>
+<?PHP
+
+    require '../../../src/starter.php';
+    File::dumpFile(
+        HEADER_HTML,
+        array(
+            "SITE_ROOT" => SITE_ROOT,
             "title" => "Linux Basics Introduction",
             "author" => "By Logan Rickert"
     ));
